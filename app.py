baefdc2ae6250d2e2e50c73215c10d71bca220af
lawiwi <dnnangelika@gmail.com>
2025-04-30 19:39:42 -0500

    Prototipo Inicial

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, send_file
+import sqlite3
+
+
+app = Flask(__name__, template_folder='Templates')
+
+# Home 
+
+@app.route("/")
+def chiaentre():
+    return render_template('Chia/iniciachia.html')
+
+@app.route("/Chia/Restaurantes")
+def chiacomida():
+    return render_template('Chia/Restaurantes.html')
+
+@app.route("/Chia/Arte")
+def chiaarte():
+    return render_template('Chia/Arte.html')
+
+@app.route("/Chia/Deportes")
+def chiadeportes():
+    return render_template('Chia/Deportes.html')
+
+@app.route("/Chia/Ocio")
+def chiaocio():
+    return render_template('Chia/Ocio.html')
+
+@app.route("/Chia/Shopping")
+def chiashopping():
+    return render_template('Chia/Shopping.html')
+
+@app.route("/Chia/Naturaleza")
+def chianatu():
+    return render_template('Chia/Recreacion.html')
